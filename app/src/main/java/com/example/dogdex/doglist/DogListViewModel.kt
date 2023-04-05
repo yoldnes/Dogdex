c8bac81f16007ec39ef29f17b7e18666f99a42ad
Ernesto Ortiz Peralta <ernestoortiz476@gmail.com>
2023-04-04 19:21:14 -0500

    Se añade el manejador de errores para la app
    Se agregan mensajes de error y ademas libreria para agregar imagenes de perros

--- app/src/main/java/com/example/dogdex/doglist/DogListViewModel.kt
+++ app/src/main/java/com/example/dogdex/doglist/DogListViewModel.kt
@@ -5,12 +5,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.dogdex.Dog
+import com.example.dogdex.api.ApiResponseState
 import kotlinx.coroutines.launch
 
 class DogListViewModel : ViewModel() {
 
     private val _dogList = MutableLiveData<List<Dog>>()
     val dogList: LiveData<List<Dog>> get() = _dogList
+
+    private val _status = MutableLiveData<ApiResponseState<List<Dog>>>()
+    val status: LiveData<ApiResponseState<List<Dog>>> get() = _status
 
     private val repository = DogRepository()
 
@@ -20,7 +24,15 @@ class DogListViewModel : ViewModel() {
 
     private fun downloadDogs() {
         viewModelScope.launch {
-            _dogList.value = repository.downloadDogs()
+            _status.value = ApiResponseState.Loading()
+            handleResponseStatus(repository.downloadDogs())
         }
+    }
+
+    private fun handleResponseStatus(apiResponseStatus: ApiResponseState<List<Dog>>) {
+        if (apiResponseStatus is ApiResponseState.Success){
+            _dogList.value = apiResponseStatus.data
+        }
+        _status.value = apiResponseStatus
     }
 }
